383702f80f09818a96c01dd60e6a378a61983e66
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-11-25 19:19:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
+<head>    
+    <title>Homepage</title>
+</head>
+
+<body>
+    <h1>Tugas CRUD</h1>
+    <p>Nama : Yudhan Jeffri Djuniartha</p>
+    <p>Kelas : XI RPL 5</p>
+    <p>No Absen : 31</p>
+    <table width='80%' border=1>
+
+    <tr>
+        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";    
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a> | <a href='add.php'>Add</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
+</html>
